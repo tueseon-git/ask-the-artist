081e21ef2b624251edfbbae306e2145a43e71d0a
tueseon-git <tueseontechnologies@gmail.com>
2025-12-04 11:26:10 +0530

    Update data.py

--- backend/app/data.py
+++ backend/app/data.py
@@ -1,3 +1,3 @@
 # Put any static data, contact link or extended KB here.
-# Right now main.py is not using ths file, instead main.py has its seperate code for this in itself.
+# Right now main.py is not using this file, instead main.py has its seperate code for this in itself.
 CONTACT_URL = "https://tueseon.com/contact-us"
