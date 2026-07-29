c45db2399dab61b64f1ee1f01b63f507023b54c1
tueseon-git <tueseontechnologies@gmail.com>
2026-07-29 15:10:24 +0530

    Update model.py

--- backend/app/model.py
+++ backend/app/model.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer, util
 
 class EmbeddingHelper:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
-    self.model = SentenceTransformer(model_name)
+        self.model = SentenceTransformer(model_name)
 
 
     def embed_list(self, texts):
